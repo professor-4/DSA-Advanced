dccf2b7237e82869d8743fae2616dff110cd5a25
Akash kushwah <63041028+professor-4@users.noreply.github.com>
2020-12-19 15:43:42 +0530

    Compiled Successfully

---
+++ 05- Searching/02- Binary Search (Recursive).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int bSearch( int *arr, int low,  int high, int k){
+	if (low>high)
+		{return -1;}
+
+	while (low<=high){
+		int mid = (low+high)/2;
+		if (arr[mid]==k){
+			return mid;
+		}
+		else if (arr[mid]<k){
+			bSearch(arr, mid+1, high, k);
+		}
+		else bSearch(arr, low , mid-1,k);
+	}
+}
+
+
+
+int main(){
+	int arr[] = {10, 20, 30, 40, 50, 60};
+	int n = 6;
+
+	int x = 20;
+	
+	cout<<"element present at index: "<<bSearch(arr, 0, n-1, x);
+	return 0;
+}
