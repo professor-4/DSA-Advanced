58d698858f1c3e482f34fa87c3910c0ef4addd71
Akash kushwah <63041028+professor-4@users.noreply.github.com>
2020-12-19 15:45:33 +0530

    Compiled Successfully

---
+++ 05- Searching/06- cont1sInsortedBinary.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int count1s(int *arr, int n){
+	int low = 0, high = n-1;
+	
+
+	while (low<=low){
+		int mid = (low+high)/2;
+		if (arr[mid]==0)
+			low = mid+1;
+		else {
+			if (mid==0|| arr[mid-1]==0){
+				return (n- mid);
+			}
+			else 
+				high = mid-1;
+		}
+	}
+	return 0;
+}
+
+int main(){
+	int arr[] = {0, 0, 1, 1, 1, 1}, n = 6;
+
+   cout << count1s(arr, n);
+
+	return 0;
+}
